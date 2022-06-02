5c59872172ef8847599c8af17049b579ee8e0369
Gonzalo <gonzalo@mail.com>
2022-06-02 12:38:02 -0500

    Settings classes

---
+++ Play.Catalog/src/Play.Catalog.Service/Settings/MongoDbSettings.cs
@@ -0,0 +1,12 @@
+namespace Play.Catalog.Service.Settings
+{
+    class MongoDbSettings{
+
+        public string Host {get; init;}
+
+        public int Port {get; init;}
+
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+
+    }
+}
