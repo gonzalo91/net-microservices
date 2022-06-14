1b10125cbbdd3a21e37361e7ec60137aaa1ced4a
Gonzalo <gonzalo@mail.com>
2022-06-14 18:52:30 -0500

    contracts library

---
+++ Play.Catalog/src/Play.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Play.Catalog.Contracts{
+
+
+    public record CatalogItemCreated(Guid ItemId, string name, string description){
+
+    }
+
+    public record CatalogItemUpdated(Guid ItemId, string name, string description){
+    }
+
+    public record CatalogItemDeleted(Guid ItemId){
+
+    }
+
+}
